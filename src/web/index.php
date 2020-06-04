2fbcf41be8a907e6450c42dd8c359f4deca615bd
Maxi-M <maxim.moskovskiy@mail.ru>
2020-06-04 11:47:52 +0300

    Some changes

--- src/web/index.php
+++ src/web/index.php
@@ -1,5 +1,19 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$a=1/0;
-phpinfo();
+
+$cache = new Memcached();
+$cache->addServer('host.docker.internal', 11311);
+$cache->set('string', "Some string");
+
+var_dump($cache->get('string'));
+
+session_start();
+
+if (isset($_GET['name'])) {
+    $_SESSION['myName'] = $_GET['name'];
+}
+
+If (isset($_SESSION['myName'])) {
+    echo "Привет ".$_SESSION['myName']."<br>";
+    echo "Session save path = ".session_save_path()."<br>";
+    echo "Session save handler = ".ini_get("session.save_handler");
+}
